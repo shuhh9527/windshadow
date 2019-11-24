d3ad3e7c2dbd21f8a73891f31971b74141eea6b6
windshadow <1506203187@qq.com>
2019-11-24 11:42:41 +0800

    增加侧边栏

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,11 +1,104 @@
 <template>
-<div>
-  <WsNavbar></WsNavbar>
-  <WsMenu></WsMenu>
+<!-- <div>
+  <div class="navbar">
+    <WsNavbar></WsNavbar>
+  </div>
+  <div>
+    <div class="sidebar">
+      <WsMenu></WsMenu>
+    </div>
+    <div class="content">
+      dsakfksakgs
+    </div>
+  </div>
   <router-view />
-</div>
+</div> -->
+<el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">
+      <el-row>
+        <el-col :span="24">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>导航一</span>
+        </template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
+  </el-col>
+      </el-row>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
-
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  } 
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
 <script>
 import WsMenu from './WsMenu'
 import WsNavbar from './WsNavbar'
@@ -19,4 +112,19 @@ export default {
 </script>
 
 <style>
+.navbar{
+  height: 80px;
+  width: 100%;
+}
+.sidebar{
+  width: 200px;
+  height: 100vh;
+}
+.content{
+  width: 100%;
+  height: 100%;
+  margin-top: 80px;
+  margin-left: 200px;
+  background-color: aqua;
+}
 </style>
